e4b47425d322161f91ea386648ecdea42a4d83d7
Omar Ahmed <Oa0578222@gmail.com>
2022-11-09 08:51:51 +0200

    Tasks

---
+++ Session_2/CPU_Driver/CPU_Interface.h
@@ -0,0 +1,18 @@
+#ifndef CPU_INTERFACE_H_
+#define CPU_INTERFACE_H_
+
+void Cpu_EnableAllInterrupts(void);
+
+void Cpu_DisableAllInterrupts(void);
+
+void Cpu_SwitchToUserMode(void);
+
+void Cpu_SwitchToPrivMode(void);
+
+void Cpu_StartCriticalSection(void);
+
+void Cpu_EndCriticalSection(void);
+
+void SVC_Handler(void);
+
+#endif
